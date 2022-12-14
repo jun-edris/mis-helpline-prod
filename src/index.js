4a8b275cf40b3b7438c6b1866e35d322042059b9
Junaire Edris B. Buico <juned.buico@gmail.com>
2022-12-14 20:32:17 +0800

    deployment

--- src/index.js
+++ src/index.js
@@ -27,12 +27,12 @@ app.use('/api', superAdminRoute);
 
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static('client/build'));
-  
-	app.get('*', (req, res) => {
-	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	app.use(express.static(path.join(__dirname, '../client/build')));
+
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, '../client/build/index.html'));
 	});
-  }
+}
 
 mongoose.set('strictQuery', true);
 
